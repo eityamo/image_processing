6bc16c1dce3163392ada7fa5d90172f99d7a825e
eityamo <eityamo@gmail.com>
2022-12-01 19:02:15 +0900

    γ変換

---
+++ ganma.py
@@ -0,0 +1,15 @@
+# Γ変換とは画像の明るさの変換方法
+import cv2
+import numpy as np
+gamma = 1.5
+img = cv2.imread("src/Berry.jpg")
+gamma_cvt = np.zeros((256,1), dtype=np.uint8)
+for i in range(256):
+  gamma_cvt[i][0] = 255 * (float(i)/255) ** (1.0 / gamma)
+
+img_gamma = cv2.LUT(img, gamma_cvt)
+
+cv2.imshow("img", img)
+cv2.imshow("gamma", img_gamma)
+cv2.waitKey(0)
+cv2.destroyAllWindows()

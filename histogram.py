ad7b73b52451cf25ded859bf6ac9ad142bce27ab
eityamo <eityamo@gmail.com>
2022-12-01 19:01:33 +0900

    ヒストグラム

---
+++ histogram.py
@@ -0,0 +1,20 @@
+import cv2
+# ヒストグラムを使うから
+import matplotlib.pyplot as plt
+# %matplotlib inline
+img = cv2.imread("src/Lena.jpg")
+# cv2.imshow("img", img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows
+
+# ヒストグラムのリスト
+color_list = ["blue", "green", "red"]
+for i,j in enumerate(color_list):
+  hist = cv2.calcHist([img], [i], None, [256], [0,256])
+  plt.plot(hist, color = j)
+
+img_gray = cv2.imread("src/Lena.jpg", 0)
+hist2 = cv2.calcHist([img_gray], [0], None, [256], [0,256])
+plt.plot(hist2)
+
+plt.show()

791954981913bff8d3b42586878f344be52bf3c8
eityamo <eityamo@gmail.com>
2022-12-02 17:04:43 +0900

    モルフォロジー演算

---
+++ morphology.py
@@ -0,0 +1,25 @@
+# モルフォロジー演算とは膨張と収縮からなる
+# 幅せるを収縮させたり、膨張させると図形を分離・一体化できる
+# オープニング・クロージングは図形の大きさを保ちながら形状操作ができる
+import cv2
+import numpy as np
+img = cv2.imread("src/floor.jpg", 0)
+ret, img_th = cv2.threshold(img, 110, 255, cv2.THRESH_BINARY)
+cv2.imshow("img", img_th)
+
+# 着目画素（3、3）を黒にするか白にするか
+kernel = np.ones((3,3), dtype=np.uint8)
+# 白
+img_d = cv2.dilate(img_th, kernel)
+# 黒
+img_e = cv2.erode(img_th, kernel)
+
+cv2.imshow("e", img_e)
+cv2.imshow("d", img_d)
+
+# クロージング
+img_c = cv2.morphologyEx(img_th, cv2.MORPH_CLOSE, kernel)
+cv2.imshow("c", img_c)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()

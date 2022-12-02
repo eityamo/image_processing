1f0ad7ba13b9e839129e6d5b4b68247f9f98f542
eityamo <eityamo@gmail.com>
2022-12-02 21:43:12 +0900

    顔検出

---
+++ face_detection.py
@@ -0,0 +1,14 @@
+import cv2
+HAAR_FILE = "/Users/eityamo/opt/anaconda3/lib/python3.9/site-packages/cv2/data/haarcascade_frontalface_default.xml"
+cascade = cv2.CascadeClassifier(HAAR_FILE)
+img = cv2.imread("src/Solvay_conference_1927.jpg")
+img_g = cv2.imread("src/Solvay_conference_1927.jpg", 0)
+
+# 画像が存在する場所を格納
+face = cascade.detectMultiScale(img_g)
+for x, y, w, h in face:
+  cv2.rectangle(img, (x,y), (x+w, y+h), (0,0,255), 1)
+
+cv2.imshow("img", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
